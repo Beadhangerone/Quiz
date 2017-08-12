c4fc952cd75b295867a0c41458a731aeab53e3b5
Beadhangerone <denm.2206@gmail.com>
2017-08-12 12:43:47 +0300

    Next path btn

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -6,10 +6,12 @@ class QuestionController < ApplicationController
     answer = Question.find(params[:id]).variants.where(is_answer: true).take()
     rightA = answer.id.to_i
     clientA = params[:answ_id].to_i
+    nextQPath = get_question_path( params[:id].to_i+1 ).to_s
 
     render json:({
         'rightA': rightA,
         'clientA': clientA,
+        'nextQPath': nextQPath,
     })
 
   end
