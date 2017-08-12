c1d0c02885fbf5e0e869a157d5ec4324fa001457
Beadhangerone <denm.2206@gmail.com>
2017-08-12 14:37:35 +0300

    activeLinks navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def recogPath(path)
+    Rails.application.routes.recognize_path(path)
+  end
+  def active(url, controller)
+    if url
+      if controller && controller_name == recogPath(url)[:controller]
+          return 'active'
+      elsif !controller && controller_name == recogPath(url)[:controller] && action_name == recogPath(url)[:action]
+          return 'active'
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get '/question/:id' => 'question#show', as: 'get_question'
-  get '/question/1' => 'question#show', as: 'start_game' # do a random question later
   post '/question/:id' => 'question#check'
 end
